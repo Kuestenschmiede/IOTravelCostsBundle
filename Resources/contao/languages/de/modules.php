377d4ecaf7d9cb0c4a7195374d88d5733c1337e9
coastforge_mei <matthias.eilers@kuestenschmiede.de>
2020-01-28 14:48:03 +0100

    bundle name in navigation now switchable in global settings

--- Resources/contao/languages/de/modules.php
+++ Resources/contao/languages/de/modules.php
@@ -11,8 +11,8 @@
  * @link       https://www.con4gis.org
  */
 
-$GLOBALS['TL_LANG']['MOD']['c4g_travel_costs_settings'] = ['(travel-costs) Einstellungen', 'Fahrtkosten-Einstellungen bearbeiten.'];
-$GLOBALS['TL_LANG']['MOD']['c4g_travel_costs_tariffs'] = ['(travel-costs) Tarife', 'Fahrtkosten-Tarif konfigurieren.'];
+$GLOBALS['TL_LANG']['MOD']['c4g_travel_costs_settings'] = ['Einstellungen', 'Fahrtkosten-Einstellungen bearbeiten.'];
+$GLOBALS['TL_LANG']['MOD']['c4g_travel_costs_tariffs'] = ['Tarife', 'Fahrtkosten-Tarif konfigurieren.'];
 
 $GLOBALS['TL_LANG']['FMD']['c4g_travel_costs'] = ['(travel-costs) Fahrtkostenrechner', 'Fahrtkostenrechner (von / bis). Erforder einen con4gis.io API-Schlüßel.'];
 $GLOBALS['TL_LANG']['FMD']['c4g_travel_costs_tariffs'] = ['(travel-costs) Fahrtkostentarife', 'Fahrtkostentarife darstellen'];
